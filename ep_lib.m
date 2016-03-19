3de02b3127f7b3c0ac74fa530c9d25b38fb20f1c
Wagner Ferreira Alves <wagnerferreirasp@hotmail.com>
2016-03-18 23:31:26 -0300

    Create ep_lib.m

---
+++ ep_lib.m
@@ -0,0 +1,37 @@
+function [V,C,L] = encontraVCL(A) 
+    [n,n] = size(A);
+    V = [];
+    C = [];
+    L = [];
+    for i=1:n
+        for j=1:n
+            if (A(i,j) != 0)
+                V = [V, A(i,j)];
+                L = [L, i];
+                C = [C, j];
+            end
+        end
+    end
+end
+
+function y = multiplica(V,C,L,x) 
+    [m,l] = size(V);
+    n = size(x);
+    y = zeros(n,1);
+    for i=1:l
+        y(L(i)) = y(L(i)) + V(i)*x(C(i));
+    end
+end
+
+function x = rotinaPrincipal(A, m) 
+    [n, n] = size(A);
+    [V,C,L] = encontraVCL(A);
+    x0 = ones(n,1)/n;
+    xAnterior = x0;
+    x = (1-m)*multiplica(V,C,L,x0) + m*x0;
+    while (norm(xAnterior-x, 1) > 1.0000e-005)
+        xAnterior = x;
+        x = (1-m)*multiplica(V,C,L, x) + m*x0;
+    end
+end
+
