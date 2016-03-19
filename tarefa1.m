223d632ce44a9567248718119cd60bfb4a93216f
Wagner Ferreira Alves <wagnerferreirasp@hotmail.com>
2016-03-19 10:37:45 -0300

    Update tarefa1.m

--- tarefa1.m
+++ tarefa1.m
@@ -1,4 +1,7 @@
+% Tarefa 1 do Exercicio-Programa: 
+% Mostrar as importancias das paginas dada sua rede
 function x = tarefa1() 
+    % A eh a matriz de ligacao da rede
     A = [0 0 0 0 0 0 0 1/2;
          1/2 0 0 0 0 0 0 0;
          1/2 1/2 0 0 0 0 0 1/2;
