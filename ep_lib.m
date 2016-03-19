31afdfddf7a8a72c6e9b1389309cabf14ea4f361
Wagner Ferreira Alves <wagnerferreirasp@hotmail.com>
2016-03-19 09:50:52 -0300

    Update ep_lib.m

--- ep_lib.m
+++ ep_lib.m
@@ -14,34 +14,34 @@ function [V,C,L] = encontraVCL(A)
     end
 end
 
-function y = multiplica(V,C,L,x) 
+function y = multiplica(V, C, L, x) 
     [m,l] = size(V);
     [z,w] = size(x);
-    y = zeros(z,1);
+    y = zeros(z, 1);
     for i=1:l
         y(L(i)) = y(L(i)) + V(i)*x(C(i));
     end
 end
 
-function x = rotinaPrincipal(A, m) 
-    [n, n] = size(A);
-    [V,C,L] = encontraVCL(A);
-    x0 = ones(n,1)/n;
-    xAnterior = x0;
-    x = (1-m)*multiplica(V,C,L,x0) + m*x0;
-    while (norm(xAnterior-x, 1) > 1.0000e-005)
-        xAnterior = x;
-        x = (1-m)*multiplica(V,C,L, x) + m*x0;
-    end
-    mostraPaginasOrdenadas(x);
-end
-
 function mostraPaginasOrdenadas(x) 
     [z,w] = size(x);
     A = [transpose([1:z]), x];
-    B = sortrows(A,-2);
+    B = sortrows(A, -2);
     printf("Paginas mais importantes: \n");
     for i=1:z
         printf("Pagina %d - Importancia: %f\n", B(i, 1), B(i, 2));
     end
+end
+
+function x = rotinaPrincipal(A, m) 
+    [n,n] = size(A);
+    [V,C,L] = encontraVCL(A);
+    x0 = ones(n, 1)/n;
+    xAnterior = x0;
+    x = (1-m)*multiplica(V, C, L, x0) + m*x0;
+    while (norm(xAnterior-x, 1) > 1e-5)
+        xAnterior = x;
+        x = (1-m)*multiplica(V, C, L, x) + m*x0;
+    end
+    mostraPaginasOrdenadas(x);
 end
